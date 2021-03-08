3a0d62d7ca004924bb005b3d2dd7d07f6708a422
yarmor20 <57010347+yarmor20@users.noreply.github.com>
2021-03-08 15:34:25 +0200

    Update add.cu

--- Example #1: Basic Notions/add.cu
+++ Example #1: Basic Notions/add.cu
@@ -9,17 +9,13 @@ __global__ void add(int *c, int *a, int *b) {
 }
 
 int main() {
-    int a, b, c; // host copies of a, b, c
+    int a = 5, b = 4, c; // host copies of a, b, c
     int *d_a, *d_b, *d_c; // device copies of a, b, c
 
     // Allocate space for device copies of a, b, c
     cudaMalloc(&d_a, sizeof(int));
     cudaMalloc(&d_b, sizeof(int));
     cudaMalloc(&d_c, sizeof(int));
-
-    // Setup input values
-    a = 2;
-    b = 7;
 
     // Copy inputs to device
     cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
@@ -33,6 +29,7 @@ int main() {
 
     // Cleanup
     cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
     printf("Result: %d\n", c);
     return 0;
 }
